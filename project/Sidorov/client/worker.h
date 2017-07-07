7c64d48718a483aca31929f7fdc92c9d369ae9dc
ZpmPower <sidorovbogdan1@gmail.com>
2017-07-07 13:34:47 +0300

    init commit

---
+++ project/Sidorov/client/worker.h
@@ -0,0 +1,29 @@
+#ifndef WORKER_H
+#define WORKER_H
+#include <asio.hpp>
+
+#include <thread>
+#include <memory>
+
+#include "define.h"
+
+class Worker
+{
+public:
+    static Worker* instance();
+
+    void start();
+
+    void join();
+
+    asio::io_service& io_service();
+
+private:
+    Worker();
+
+    asio::io_service service_;
+
+    std::vector<std::shared_ptr<std::thread>> threadPool_;
+};
+
+#endif // WORKER_H
