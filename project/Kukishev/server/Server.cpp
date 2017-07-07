a0922d7d9ba23d393a7db9d7e66a10b00135c8f7
Predator051 <aleksey.kukishev@gmail.com>
2017-07-07 15:07:53 +0300

    Created class session and mod class server

--- project/Kukishev/server/Server.cpp
+++ project/Kukishev/server/Server.cpp
@@ -7,7 +7,23 @@ Server::Server(int port)
     : io_service_(Worker::instance()->io_service())
     , acceptor_( io_service_
           , asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port))
+    , socket_(io_service_)
 {
 
+}
+
+void Server::start_accept()
+{
+    acceptor_.async_accept(socket_, [this](asio::error_code error)
+    {
+        if(!error)
+        {
+            SessionPtr session = Session::getNewSessions(socket_);
+            sessions_.push_back(session);
+            session->start();
+        }
+
+        start_accept();
+    });
 }
 
---
+++ project/Kukishev/server/Session.cpp
@@ -0,0 +1,40 @@
+#include "Session.h"
+
+Session::Session(asio::ip::tcp::socket& socket)
+    : socket_(socket)
+{
+    buffer_.resize(BUFFER_MAX_SIZE);
+}
+
+std::shared_ptr<Session> Session::getNewSessions(asio::ip::tcp::socket &socket)
+{
+    SessionPtr session(new Session(socket));
+    return session;
+}
+
+void Session::start()
+{
+    asio::async_read(socket_
+                     , asio::buffer(buffer_)
+                     , std::bind(&Session::handle_read
+                                 , shared_from_this()
+                                 , std::placeholders::_1
+                                 , std::placeholders::_2));
+}
+
+void Session::handle_read(asio::error_code error, size_t bufferSize)
+{
+    if(!error)
+    {
+        //process message
+        buffer_.resize(bufferSize);
+        LOG_INFO("Message: [" /*<< buffer_*/ << "]");
+
+        start();
+    }
+    else
+    {
+        LOG_ERR("Failure: read error code " << error.value()
+                << " description: " << error.message());
+    }
+}
